676c8e1f3a77eda94ba4546d60928b7bbe1690ae
kiarf <kiar.fatah@gmail.com>
2019-06-09 13:31:10 +0200

    Create mergesort.py

---
+++ mergesort.py
@@ -0,0 +1,29 @@
+#Merge sort
+#Kiar Fatah
+import numpy as np
+
+
+#help_func is needed to call is_list_sorted
+def help_func(arr):
+    if len(arr) = 1:
+        return True
+    else:
+        r = len(arr)
+        return is_list_sorted(mergesort(arr,l = 0,r))
+    
+
+
+#Check if the list is sorted for testing
+def is_list_sorted():
+
+
+def mergesort(arr,l,r):
+    while 
+
+
+
+#Test
+if __name__ == "__main__":
+
+for i in range(1,100):
+    assert help_func(np.random.rand(i,1)) == True
